2f3d56fab1b4429201239bdfbe053ca0c676039e
artrathi <artrathi@gmail.com>
2022-01-09 21:27:57 -0500

    JS Object Cricket team

---
+++ javascript/jsObjectCricket.js
@@ -0,0 +1,28 @@
+const India={
+    captains:["Virat Kohli","Rohit Sharma"],
+Coach:"Rahul Dravid",
+jerseyColor:"Sky Blue"
+}
+const Pakistan={
+    captains:["Babar Azam"],
+Coach:"Waqar Younis",
+jerseyColor:"Green"
+}
+const England={
+    captains:["Joe Root","Eoin Morgan"],
+Coach:"Chris Silverwood",
+jerseyColor:"Blue"
+}
+const NewZealand={
+    captains:["Kane Williamson"],
+Coach:"Gary Stead",
+jerseyColor:"Black"
+}
+const SouthAfrica={
+    captains:[" Quinton de Kock"],
+Coach:"Mark Boucher",
+jerseyColor:"Green"
+}
+console.log("Indian Cricket team Color: "+India.jerseyColor);
+console.log("England team Captain: "+England.captains);
+console.log("New Zealand team Head Coach: "+NewZealand.Coach);
