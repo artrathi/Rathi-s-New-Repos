0ce7b5f5b74b9c2b4d0afa1a53a6624555f13437
artrathi <artrathi@gmail.com>
2022-01-09 20:44:22 -0500

    Count Char in String

---
+++ javascript/countCharStr.js
@@ -0,0 +1,8 @@
+let enterString="Hello World!";
+let len;
+function stringLength(enterString)
+{
+len=enterString.length;
+return(len);
+}
+console.log("The Number of characters in the string is: "+stringLength(enterString));
