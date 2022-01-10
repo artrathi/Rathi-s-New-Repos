9b1a832a5d51b109a5d12223a3e5f60024d7548c
artrathi <artrathi@gmail.com>
2022-01-09 20:48:27 -0500

    Cube Root

---
+++ javascript/cubeRoot.js
@@ -0,0 +1,13 @@
+let num=8;
+
+function findCuberoot(num)
+{
+return(Math.cbrt(num));
+}
+console.log("The Cube root of "+num+" is "+findCuberoot(num));
+num=27;
+console.log("The Cube root of "+num+" is "+findCuberoot(num));
+num=64;
+console.log("The Cube root of "+num+" is "+findCuberoot(num));
+num=125;
+console.log("The Cube root of "+num+" is "+findCuberoot(num));
