2cb5cc3649ac3160d7db24029408ea5a80f671ad
artrathi <artrathi@gmail.com>
2022-01-09 20:41:39 -0500

    Json US states

---
+++ javascript/usStatesJsonFile.js
@@ -0,0 +1,102 @@
+"usStates":[
+{
+	"stateName":"Virginia",
+  "stateCapital":"Richmond",
+  "stateWebsite":"www.virginia.gov",
+  "stateGovernor":"Ralph Northam.D ",
+  "statePopulation":"8,411,808", 
+  "stateCounties":"95",
+  "stateLarCounty":"Fairfax",
+  "stateLandArea":"42,774.2sqmi"
+},
+{
+	"stateName":"Maryland",
+  "stateCapital":"Annapolis",
+  "stateWebsite":"www.maryland.gov",
+  "stateGovernor":"Larry Hogan,R",
+  "statePopulation":"6,006,401", 
+  "stateCounties":"23",
+  "stateLarCounty":"Montgomery",
+  "stateLandArea":"9,774 sq mi"
+},
+{
+	"stateName":"California",
+  "stateCapital":"Sacramento",
+  "stateWebsite":"www.california.gov",
+  "stateGovernor":"Jerry Brown.D",
+  "statePopulation":"39,144,818", 
+  "stateCounties":"58",
+  "stateLarCounty":"Los Angeles",
+  "stateLandArea":"155,959 sq mi"
+},
+{
+	"stateName":"New Jersey",
+  "stateCapital":"Trenton",
+  "stateWebsite":"www.newjersey.gov",
+  "stateGovernor":"Chris Christie.R",
+  "statePopulation":"8,944,469", 
+  "stateCounties":"21",
+  "stateLarCounty":"Bergen",
+  "stateLandArea":"8,722.58 sq mi"
+},
+{
+	"stateName":"New York",
+  "stateCapital":"Albany",
+  "stateWebsite":"www.newyork.gov",
+  "stateGovernor":"Andrew Cuomo.D",
+  "statePopulation":"19,795,791", 
+  "stateCounties":"62",
+  "stateLarCounty":"Kings",
+  "stateLandArea":"54,555sqmi"
+},
+{
+	"stateName":"Iowa",
+  "stateCapital":"Des Moines",
+  "stateWebsite":"www.iowa.gov",
+  "stateGovernor":"Kim Reynolds.R",
+  "statePopulation":"3,123,899 ", 
+  "stateCounties":"99",
+  "stateLarCounty":"Polk",
+  "stateLandArea":" 55,869 sq mi"
+},
+{
+	"stateName":"Illinois",
+  "stateCapital":"Springfield",
+  "stateWebsite":"www.illinoise.gov",
+  "stateGovernor":"Bruce Rauner.R",
+  "statePopulation":"12,859,995", 
+  "stateCounties":"102",
+  "stateLarCounty":"Cook",
+  "stateLandArea":"555,584 sq m"
+},
+{
+	"stateName":"Wisconsin",
+  "stateCapital":"Madison",
+  "stateWebsite":"www.wisconsin.gov",
+  "stateGovernor":"Scott Walker.R",
+  "statePopulation":"5,778,708", 
+  "stateCounties":"72",
+  "stateLarCounty":"Milwaukee",
+  "stateLandArea":"65,498.37sqmi"
+},
+{
+	"stateName":"Texas",
+  "stateCapital":"Austin",
+  "stateWebsite":"www.texas.gov",
+  "stateGovernor":"Greg Abbott.R",
+  "statePopulation":"27,695,284", 
+  "stateCounties":"252",
+  "stateLarCounty":"Harris",
+  "stateLandArea":"268,581sqmi"
+},
+{
+	"stateName":"Florida",
+  "stateCapital":"Tallahassee",
+  "stateWebsite":"www.florida.gov",
+  "stateGovernor":"Rick Scott.R",
+  "statePopulation":"20,271,272", 
+  "stateCounties":"67",
+  "stateLarCounty":"Miami-Dade",
+  "stateLandArea":"53,927 sq mi"
+}
+]
